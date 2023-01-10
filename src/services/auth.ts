6a9f1a9a774bdbf23699d630aca85608fe8bee09
amorïm <63107417+amorimll@users.noreply.github.com>
2023-01-10 15:02:48 -0300

    Update auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,30 +2,22 @@ import axios from "axios";
 import { Login, Register } from "../@types/services/auth.type";
 
 const login: Login = async (username, password) => {
-  try {
-    const res = await axios.post("https://sharenergy-server-production.up.railway.app/auth/login", {
-      username,
-      password,
-    });
+  const res = await axios.post("http://localhost:3001/auth/login", {
+    username,
+    password,
+  });
 
-    return res;
-  } catch (error: any) {
-    console.log(error);
-  }
+  return res;
 };
 
 const register: Register = async (username, email, password) => {
-  try {
-    const res = await axios.post("https://sharenergy-server-production.up.railway.app/auth/register", {
-      username,
-      email,
-      password,
-    });
+  const res = await axios.post("http://localhost:3001/auth/register", {
+    username,
+    email,
+    password,
+  });
 
-    return res;
-  } catch (error: any) {
-    console.log(error);
-  }
+  return res;
 };
 
 export { login, register };
